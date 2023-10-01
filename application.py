0a11898e52e327c50f3055c28a800c0f9a8e710c
MichaelDacanay <MichaelDacanay@users.noreply.github.com>
2023-10-01 20:45:07 +0000

    code formatting by black

--- application.py
+++ application.py
@@ -206,10 +206,10 @@ def calories():
     """
     Display and update calorie data.
 
-    This route handles the display (calories.html) and submission of 
-    calorie data using a form. If the form is submitted successfully, the data 
+    This route handles the display (calories.html) and submission of
+    calorie data using a form. If the form is submitted successfully, the data
     is fetched and updated in the database.
-    
+
     Input:
     - POST request with form data including email, date, food, and burnout.
 
