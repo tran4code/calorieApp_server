2402a61fb13cf95aef0672eda220412803997430
LoganWilliams1 <loganwilliamslw1@gmail.com>
2023-10-20 00:36:36 -0400

    debug cov report issue

--- src/application.py
+++ src/application.py
@@ -18,7 +18,7 @@ from flask import (
 from flask_mail import Mail
 from flask_pymongo import PyMongo
 from tabulate import tabulate
-from forms import (
+from src.forms import (
     FoodForm,
     HistoryForm,
     RegistrationForm,
